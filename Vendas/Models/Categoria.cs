8eaf39f5339258e953159dfbf7a3f5890a82e5ea
Gabriel Damico <gabrieldw@hotmail.com.br>
2022-06-14 14:20:19 -0300

    Add project files.

---
+++ Vendas/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace Vendas.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        //Relacionamento de Categoria para Lanche (uma categoria tem muitos lanches)
+        public List<Lanche> Lanches { get; set; }
+
+    }
+}
---
+++ Vendas/Models/Lanche.cs
@@ -0,0 +1,20 @@
+﻿namespace Vendas.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        //Relacionamento de Lanche para Categoria
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+
+    }
+}
