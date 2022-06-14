d42cc426c5a96b8b3af0d4620b0274d5caefb43c
Gabriel Damico <gabrieldw@hotmail.com.br>
2022-06-14 18:18:50 -0300

    Adicionado DataAnnotations as classes para melhor construção do banco// Adicionado conexão ao banco Sql

--- Vendas/Models/Categoria.cs
+++ Vendas/Models/Categoria.cs
@@ -1,9 +1,18 @@
-﻿namespace Vendas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vendas.Models
 {
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+        [StringLength(100,ErrorMessage="O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage ="Informe o nome da categoria")]
+        [Display(Name = "Nome")]
         public string CategoriaNome { get; set; }
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
 
         //Relacionamento de Categoria para Lanche (uma categoria tem muitos lanches)
--- Vendas/Models/Lanche.cs
+++ Vendas/Models/Lanche.cs
@@ -1,20 +1,42 @@
-﻿namespace Vendas.Models
-{
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vendas.Models
+{    
     public class Lanche
     {
+        [Key]        
         public int LancheId { get; set; }
+        [StringLength(80,MinimumLength =10, ErrorMessage = "O tamanho máximo é 80 caracteres")]
+        [Required(ErrorMessage = "Informe o nome do lanche deve ser informado")]
+        [Display(Name = "Nome do Lanche")]
         public string Nome { get; set; }
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "minimo de 10 maximo de 200")]
+        [Required(ErrorMessage = "Descrição deve ter no mínimo 10 caracteres")]
+        [Display(Name = "Descrição do Lanche")]
         public string DescricaoCurta { get; set; }
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "minimo de 20 maximo de 200")]
+        [Required(ErrorMessage = "Descrição deve ter no mínimo 20 caracteres")]
+        [Display(Name = "Descrição detalhada do Lanche")]
         public string DescricaoDetalhada { get; set; }
+        [Required(ErrorMessage = "Informe o preço")]
+        [Display(Name = "Preço")]
+        [Column(TypeName ="decimal(10,2)")]
+        [Range(1,999.99,ErrorMessage ="Preço deve estar entre 1 e 999,99")]
         public decimal Preco { get; set; }
+        [StringLength(200, ErrorMessage = "maximo de 200 caract")]       
+        [Display(Name = "Caminho imagem")]
         public string ImagemUrl { get; set; }
+        [StringLength(200, ErrorMessage = "maximo de 200 caract")]
+        [Display(Name = "Caminho imagem miniatura")]
         public string ImagemThumbnailUrl { get; set; }
+        [Display(Name = "Preferido?")]
         public bool IsLanchePreferido { get; set; }
+        [Display(Name = "Em estoque?")]
         public bool EmEstoque { get; set; }
 
         //Relacionamento de Lanche para Categoria
         public int CategoriaId { get; set; }
         public virtual Categoria Categoria { get; set; }
-
     }
 }
