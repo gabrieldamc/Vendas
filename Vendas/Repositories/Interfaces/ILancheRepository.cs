0925f0e58e4cc5872f396a6ba5ec83667bc69f75
Gabriel Damico da Fonseca <gabrieldw@hotmail.com.br>
2022-06-15 20:24:14 -0300

    Implementando padrão Repository para Lanches

---
+++ Vendas/Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,12 @@
+﻿using Vendas.Context;
+using Vendas.Models;
+
+namespace Vendas.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPreferidos { get; }        
+        Lanche GetLancheById(int Lancheid);
+    }
+}
---
+++ Vendas/Repositories/LancheRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Vendas.Context;
+using Vendas.Models;
+using Vendas.Repositories.Interfaces;
+
+namespace Vendas.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+        public LancheRepository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches.Where(p => p.IsLanchePreferido).Include(c => c.Categoria);
+
+        public Lanche GetLancheById(int Lancheid)
+        { 
+            return _context.Lanches.FirstOrDefault(I => I.LancheId == Lancheid);
+        }
+        
+    }
+}
