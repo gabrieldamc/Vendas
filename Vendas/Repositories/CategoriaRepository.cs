a34f7e2eba9b06a1845c4918418ff051c9f5d450
Gabriel Damico da Fonseca <gabrieldw@hotmail.com.br>
2022-06-15 19:56:17 -0300

    Implementando padrão Repository para Categorias

---
+++ Vendas/Repositories/CategoriaRepository.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Vendas.Context;
+using Vendas.Models;
+using Vendas.Repositories.Interfaces;
+
+namespace Vendas.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
+    }
+}
---
+++ Vendas/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using Vendas.Models;
+
+namespace Vendas.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
