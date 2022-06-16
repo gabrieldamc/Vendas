0a0a56ef38f8d2281cfb9bb98749f52809bee793
Gabriel Damico da Fonseca <gabrieldw@hotmail.com.br>
2022-06-16 17:36:43 -0300

    Criando controler e view para Lanches

---
+++ Vendas/Controllers/LancheController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vendas.Repositories.Interfaces;
+
+namespace Vendas.Controllers
+{
+    public class LancheController : Controller
+    {
+        public readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
